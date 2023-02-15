65bb93b02ed3fbb9fd394e16424b360df490f1dc
OrkWard <orkward_o@outlook.com>
2023-02-16 07:24:13 +0800

    init vue

---
+++ FrontEnd/src/Layout.vue
@@ -0,0 +1,5 @@
+<template>
+    <p>Layer</p>
+    <p>Map</p>
+    <p>NavBar</p>
+</template>
--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -1,1 +1,5 @@
-console.log('hello, world!')
+import Layout from './Layout.vue'
+import { createApp } from 'vue'
+
+let app = createApp(Layout);
+app.mount('#app');
