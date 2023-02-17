340466d566448e56af0c4f4b9e2de246d16f8237
OrkWard <orkward_o@outlook.com>
2023-02-17 16:46:47 +0800

    init flex layout and layer/navBar component

---
+++ FrontEnd/src/Layer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Layer</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
--- FrontEnd/src/Layout.vue
+++ FrontEnd/src/Layout.vue
@@ -1,5 +1,47 @@
 <template>
-    <p>Layer</p>
-    <p>Map</p>
-    <p>NavBar</p>
+    <NavBar id="navbar"/>
+    <div id="main-container">
+        <Layer/>
+        <div id="map"></div>
+    </div>
 </template>
+
+<style scoped>
+#navbar {
+    background-color: aqua;
+    height: 50px;
+    display: flex;
+    align-items: center;
+}
+
+#main-container {
+    display: flex;
+    align-items: stretch;
+    flex-grow: 1;
+}
+
+#map {
+    flex-grow: 1;
+}
+</style>
+
+<script>
+import Layer from './Layer.vue';
+import NavBar from './NavBar.vue';
+import L from 'leaflet';
+
+export default {
+    data() {
+        return {};
+    },
+    methods: {
+
+    },
+    mounted() {
+        let map = L.map('map');
+    },
+    components: {
+        Layer, NavBar
+    }
+}
+</script>
